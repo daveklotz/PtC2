d7e651e6dd33c1cabc2831f6aee2b1547cea08aa
David Klotz <davidklotz@lamont.lan>
2020-06-30 22:24:12 -0700

    Add apple project

---
+++ PunchTheClown/Shared/PunchTheClownApp.swift
@@ -0,0 +1,17 @@
+//
+//  PunchTheClownApp.swift
+//  Shared
+//
+//  Created by David Klotz on 6/30/20.
+//
+
+import SwiftUI
+
+@main
+struct PunchTheClownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
