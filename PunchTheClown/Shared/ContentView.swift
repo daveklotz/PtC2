74488cfbebfbd0ef44a2e90c8b25e577051a673f
David Klotz <klotz@pobox.com>
2020-07-03 12:56:02 -0700

    Add a bunch of stuff

--- PunchTheClown/Shared/ContentView.swift
+++ PunchTheClown/Shared/ContentView.swift
@@ -20,28 +20,109 @@ struct ContentView: View {
     @State var xMove:CGFloat = 50.0
     @State var yMove:CGFloat = 50.0
     @State var badCounter:Int = 0
+    @State var oofCounter:Int = 0
+    @State var bigTopSound: AVAudioPlayer?
+    @State var punchSound: AVAudioPlayer?
+    @State var groan1Sound: AVAudioPlayer?
+    @State var groan2Sound: AVAudioPlayer?
+    @State var groan3Sound: AVAudioPlayer?
+    
     
     var body: some View {
-        VStack {
-            Image(clownImage)
-                .resizable()
-                .frame(width: 302, height: 450, alignment: .center)
-                .aspectRatio(contentMode: .fill)
-                .onTapGesture {
-                    self.clownImage = "punchedclown"
-                    //playPunch()
-                    //playOh()
+        GeometryReader { geometry in
+            ZStack {
+                Image("background")
+                    .resizable()
+                    .aspectRatio(geometry.size, contentMode: .fill)
+                    .edgesIgnoringSafeArea(.all)
+                Text("Score: \(oofCounter)")
+                    .position(x: 50, y: 10)                    
+                VStack {
+                    Image(clownImage)
+                        .resizable()
+                        .frame(width: 302, height: 450, alignment: .center)
+                        .aspectRatio(contentMode: .fill)
+                        .onTapGesture {
+                            self.clownImage = "punchedclown"
+                            
+                            let punchPath = Bundle.main.path(forResource: "punch.wav", ofType:nil)!
+                            let url = URL(fileURLWithPath: punchPath)
+                            
+                            do {
+                                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+                                try AVAudioSession.sharedInstance().setActive(true)
+                                
+                                punchSound =  try AVAudioPlayer(contentsOf: url)
+                                #if targetEnvironment(simulator)
+                                // do nothing
+                                #else
+                                punchSound?.play()
+                                #endif
+                                
+                                #if targetEnvironment(simulator)
+                                // do nothing
+                                #else
+                                if oofCounter % 3 == 0 {
+                                    groan1Sound?.play()
+                                } else if oofCounter % 3 == 1 {
+                                    groan2Sound?.play()
+                                } else {
+                                    groan3Sound?.play()
+                                }
+                                #endif
+                                oofCounter = oofCounter + 1
+                            } catch {
+                                // couldn't load file :(
+                            }
+                        }
+                    
+                    
+                    
                 }
-                
-                
-
+                .position(clownPosition)
+                .animation(.linear)
+                .onReceive(theTimer) { (_) in
+                    roam()
+                    
+                }
+                .onAppear {
+                    var punchPath = Bundle.main.path(forResource: "circus.wav", ofType:nil)!
+                    var url = URL(fileURLWithPath: punchPath)
+                    
+                    do {
+                        try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+                        try AVAudioSession.sharedInstance().setActive(true)
+                        
+                        bigTopSound =  try AVAudioPlayer(contentsOf: url)
+                        bigTopSound?.numberOfLoops = -1
+                        #if targetEnvironment(simulator)
+                        // do nothing
+                        #else
+                        bigTopSound?.play()
+                        #endif
+                        
+                        punchPath = Bundle.main.path(forResource: "groan1.m4a", ofType:nil)!
+                        url = URL(fileURLWithPath: punchPath)
+                        groan1Sound = try AVAudioPlayer(contentsOf: url)
+                        
+                        punchPath = Bundle.main.path(forResource: "groan2.m4a", ofType:nil)!
+                        url = URL(fileURLWithPath: punchPath)
+                        groan2Sound = try AVAudioPlayer(contentsOf: url)
+                        
+                        punchPath = Bundle.main.path(forResource: "groan3.m4a", ofType:nil)!
+                        url = URL(fileURLWithPath: punchPath)
+                        groan3Sound = try AVAudioPlayer(contentsOf: url)
+                        
+                    } catch {
+                        // couldn't load file :(
+                    }
+                }
+            }
         }
-        .position(clownPosition)
-        .animation(.linear)
-        .onReceive(theTimer) { (_) in
-            roam()
-        }
+        .statusBar(hidden: true)
+        
     }
+ 
     
     func roam() -> Void {
         
@@ -67,7 +148,7 @@ struct ContentView: View {
         self.clownPosition.y = self.clownPosition.y + self.yMove
         
         badCounter += 1
-        print(badCounter)
+        //print(badCounter)
         if badCounter > 4 {
             self.clownImage = "clown"
             badCounter = 0;
@@ -87,12 +168,37 @@ struct ContentView: View {
             try AVAudioSession.sharedInstance().setActive(true)
             
             punchSound =  try AVAudioPlayer(contentsOf: url)
-            punchSound?.play()
+            //punchSound?.play()
         } catch {
             // couldn't load file :(
         }
     }
+    
+    func playBigtop() -> Void {
+        
+
+
+        let punchPath = Bundle.main.path(forResource: "circus.wav", ofType:nil)!
+        let url = URL(fileURLWithPath: punchPath)
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            punchSound =  try AVAudioPlayer(contentsOf: url)
+            #if targetEnvironment(simulator)
+            // do nothing
+            #else
+            punchSound?.play()
+            #endif
+        } catch {
+            // couldn't load file :(
+        }
+        
+    }
+    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
