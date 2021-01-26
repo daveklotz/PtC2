9284570973cd352f821aaca9cd9027a88772e1e3
David Klotz <klotz@pobox.com>
2021-01-26 13:45:04 -0800

    some assets, bug fixes

--- PunchTheClown/Shared/Views/FortuneView.swift
+++ PunchTheClown/Shared/Views/FortuneView.swift
@@ -17,50 +17,78 @@ struct FortuneView: View {
 //    }
     var body: some View {
         if showIntro == true {
-            Text("Every 20 punches Punch will tell you your fortune! After 100 punches you'll be able to get a fortune whenever you like!!")
-                .padding(.leading, 50)
-                .padding(.trailing, 50)
+            ZStack {
+                VStack {
+                    Image("clowngenie")
+                        .scaledToFill()
+                        //.frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .bottom)
+                        .opacity(0.4)
+                    Spacer()
+                }
+                Text("Every 20 punches Punch will tell you your fortune! After 100 punches you'll be able to get a fortune whenever you like!!")
+                    .padding(.leading, 50)
+                    .padding(.trailing, 50)
+            }
 //                .onAppear { self.showWhatsNew = false }
         } else {
             let fortune = FortuneTeller.shared.getFortune()
             
-            VStack {
-                Spacer()
-                Text(fortune.description)
-                    .padding(.leading, 50)
-                    .padding(.trailing, 50)
-                    
-                Spacer()
-                Text("Was this fortune useful?")
-                HStack {
-                    Spacer()
-                    Button(action: {
-                        FortuneTeller.shared.fortuneHelpful(fortune: fortune, wasHelpful: true)
-                        // Dismiss
-                        self.isPresented = nil
-                    
-                    }) {
-                        Text("Yes!")
-                    }
-                    .padding(.bottom, 50)
-                    .padding(.top, 50)
-                    .transition(.move(edge: .trailing))
-                    .animation(.default)
-                    Spacer()
-                    Button(action: {
-                        FortuneTeller.shared.fortuneHelpful(fortune: fortune, wasHelpful: false)
-                        //Dismiss
-                        self.isPresented = nil
+            ZStack {
+                VStack {
+                    Image("clowngenie")
+                        .resizable()
+                        .scaledToFit()
                         
-                    }) {
-                        Text("No...")
-                    }
-                    .padding(.bottom, 50)
-                    .padding(.top, 50)
-                    .transition(.move(edge: .trailing))
-                    .animation(.default)
-                    
+                        //.frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .bottom)
+                        .opacity(0.4)
                     Spacer()
+                }
+                VStack {
+                    Spacer()
+                    Text(fortune.description)
+                        .font(.headline)
+                        .frame(maxWidth: .infinity)
+//                        .fixedSize()
+                        .minimumScaleFactor(0.01)
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+//                        .frame(width: 100, height: 100, alignment: .center)
+                        
+                        
+                        
+                        
+                    Spacer()
+                    Text("Was this fortune useful?")
+                        .padding()
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            FortuneTeller.shared.fortuneHelpful(fortune: fortune, wasHelpful: true)
+                            // Dismiss
+                            self.isPresented = nil
+                        
+                        }) {
+                            Text("Yes!")
+                        }
+                        .padding()
+                        .padding()
+                        .transition(.move(edge: .trailing))
+                        .animation(.default)
+                        Spacer()
+                        Button(action: {
+                            FortuneTeller.shared.fortuneHelpful(fortune: fortune, wasHelpful: false)
+                            //Dismiss
+                            self.isPresented = nil
+                            
+                        }) {
+                            Text("No...")
+                        }
+                        .padding(.bottom, 50)
+                        .padding(.top, 50)
+                        .transition(.move(edge: .trailing))
+                        .animation(.default)
+                        
+                        Spacer()
+                    }
                 }
             }
         }
--- PunchTheClown/Shared/Views/PersonalityView.swift
+++ PunchTheClown/Shared/Views/PersonalityView.swift
@@ -13,20 +13,36 @@ struct PersonalityView: View {
     @State var warning: String = ""
     
     var body: some View {
-        VStack {
-            Spacer()
-            Text(warning)
-                .padding()
-            Spacer()
-            Text(judgement)
-                .padding()
-            Spacer()
-            
-        }// end Vstack
-        .onAppear() {
-            warning = PersonalityJudge.shared.provideWarning()
-            judgement = PersonalityJudge.shared.provideJudgement()
-        }
+        ZStack {
+            VStack {
+                Image("clownshrink")
+                    .resizable()
+                    .scaledToFit()
+                    //.frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .bottom)
+                    .opacity(0.4)
+                Spacer()
+            }
+            VStack {
+                Spacer()
+                Text(warning)
+                    .font(.headline)
+                    .padding()
+                   .background(Color(.blue).opacity(0.5))
+                    .cornerRadius(20.0)
+                Spacer()
+                Text(judgement)
+                    .font(.headline)
+                    .padding()
+                    .background(Color(.red).opacity(0.5))
+                     .cornerRadius(20.0)
+                Spacer()
+                
+            }// end Vstack
+            .onAppear() {
+                warning = PersonalityJudge.shared.provideWarning()
+                judgement = PersonalityJudge.shared.provideJudgement()
+            }
+        } // end Zstack
     }
     
     
